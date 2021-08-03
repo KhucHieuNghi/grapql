f90a4e791d294f6707ea8beeb29cf1660c784851
KhucHieuNghi2512 <32290857+KhucHieuNghi@users.noreply.github.com>
2021-08-04 01:14:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server');
+// const { ApolloServer } = require('apollo-server-express');
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
